7f6c425553a40ff49e304b4ae85ec42f537a9eef
iamsumanth19 <tonysumanth19@gmail.com>
2024-10-29 18:19:39 +0530

    practice

---
+++ 27-oct-24/whatsMsg.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg("+919390752466","oyee,kottaga coding rasi try chesaa,manam ye time set cheste appude send avuthundi msg",14,46)
---
+++ 29-oct-24/practice-1.py
@@ -0,0 +1,6 @@
+def rev_str(str):
+ rev=str[::-1]
+ print(rev)
+
+string=input("Enter A String==>")
+rev_str(string)
